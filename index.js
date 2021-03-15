1ddedfa4934ed9603563cf429a6d33cacda541bc
Arshad <mohammad.1610093@kiet.edu>
2021-03-15 23:35:12 +0530

    index.js file

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const { NODE_PORT, NODE_URI } = require("./config");
 const { getDBConnection } = require("./config/dbConnection");
 getDBConnection();
+const errorMiddleware = require("./middlewares/errorHandlers/errorMiddleware");
 
 const app = express();
 require("./config/dbConnection");
@@ -11,6 +12,8 @@ app.use(cors());
 app.get("/", (req, res, next) => {
   res.status(200).send("Hello world");
 });
+
+app.use(errorMiddleware);
 
 app.listen(NODE_PORT, () => {
   console.log(`Listening at ${NODE_URI}`);
